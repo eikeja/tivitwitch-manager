c09d37ca4b8ca2281aaebdbeefbeb897bbe75a05
eikeja <101065709+eikeja@users.noreply.github.com>
2025-12-31 00:12:49 +0100

    feat: remove hls-segment-timeout and stream-timeout options, add ringbuffer-size option

--- streaming.py
+++ streaming.py
@@ -288,9 +288,8 @@ def play_live_stream_xc(username, password, stream_id, ext=None):
     session = streamlink.Streamlink()
     session.set_option("hls-live-edge", 6)
     session.set_option("hls-segment-threads", 4)
-    session.set_option("hls-segment-timeout", 10.0)
     session.set_option("hls-playlist-reload-attempts", 3)
-    session.set_option("stream-timeout", 15.0)
+    session.set_option("ringbuffer-size", "16M")
     session.set_option("http-header", "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     
     try:
@@ -331,9 +330,8 @@ def play_live_m3u(stream_id):
     session = streamlink.Streamlink()
     session.set_option("hls-live-edge", 6)
     session.set_option("hls-segment-threads", 4)
-    session.set_option("hls-segment-timeout", 10.0)
     session.set_option("hls-playlist-reload-attempts", 3)
-    session.set_option("stream-timeout", 15.0)
+    session.set_option("ringbuffer-size", "16M")
     session.set_option("http-header", "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     
     try:
